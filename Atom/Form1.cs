16e2e03b41ba2dfd44abc98bcbeb8139f4e23b49
omerfruk <omer.fruk3547@gmail.com>
2020-05-17 16:53:28 +0300

    baslangıc verileri eklendi

---
+++ Atom/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Atom
+{   
+
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();  // openfiledialog ekranını açıyoruz.
+            file.InitialDirectory = "C:";
+            string DosyaYolu = file.FileName;
+            
+            FileStream fs = new FileStream(DosyaYolu, FileMode.Open, FileAccess.Read);
+            //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
+            //2.parametre dosyanın açılacağını,
+            //3.parametre dosyaya erişimin veri okumak için olacağını gösterir.
+            StreamReader sw = new StreamReader(fs);
+            //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
+            string yazi = sw.ReadLine();
+            while (yazi != null)
+            {
+                listBox1.Items.Add(yazi);
+                yazi = sw.ReadLine();
+            }
+            //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
+            //Son satır okunduktan sonra okuma işlemini bitirdik
+            sw.Close();
+            fs.Close();
+           
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+    
+        }
+    }
+    }
+	
+
